5405d301a06320ee1f9ff5b5b39115fa2e7dd3d7
pintorig <pintori@link.it>
2021-06-08 23:22:31 +0200

    Impostata variabile ID_TORNEO per le chiamate ai servizi.

---
+++ webconsole/src/main/java/worldcup/core/utils/TorneoConfig.java
@@ -0,0 +1,11 @@
+package worldcup.core.utils;
+
+public class TorneoConfig {
+	
+	public static final String ID_TORNEO_DEFAULT = "EURO2021";
+	
+	public static final String USERNAME = "EURO2021";
+	public static final String PASSWORD = "EURO2021";
+
+	public static final String API_BASE_URL = "http://127.0.0.1:8080/api-worldcup/api/v1";
+}
