341bda4c96a8baa8d36d3886f78cda94fe691a3f
Giovanni Bussu <bussu@link.it>
2021-06-25 00:31:08 +0200

    aggiustamento

--- src/main/resources/database/sql/postgresql/patch_4_ranking_di_aggiustamento.sql
+++ src/main/resources/database/sql/postgresql/patch_4_ranking_di_aggiustamento.sql
@@ -1,4 +1,7 @@
 update squadre set ranking_fifa = 1568 where nome = 'Galles';
 update squadre set ranking_fifa = 1570 where nome = 'Polonia';
 update squadre set ranking_fifa = 1632 where nome = 'Russia';
+update squadre set ranking_fifa = 1569 where nome = 'Turchia';
+update squadre set ranking_fifa = 1667 where nome = 'Russia';
+update squadre set ranking_fifa = 1570 where nome = 'Ucraina';
 
